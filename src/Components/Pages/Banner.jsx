bffd691ca9b31bb19892bb928bb64a388db6708f
jobayer58 <jobayerafnan770@gmail.com>
2025-09-19 22:15:11 +0600

    navbar and banner design with responsive

--- src/Components/Pages/Banner.jsx
+++ src/Components/Pages/Banner.jsx
@@ -1,39 +1,55 @@
 import React from 'react';
 import banner from '../../assets/images/hero-bg.jpg'
 import rightEnd from '../../assets/images/hero-bg-shape.png'
-import rightEnd2 from '../../assets/images/hero-banner-bg.png'
-import burger from '../../assets/images/hero-banner.png'
+import rightEnd2 from '../../assets/images/hero-banner-bg-edit.png'
+import burger from '../../assets/images/hero-banner-edit.png'
 
 const Banner = () => {
     return (
-        <div className=''>
-
-            <div className='relative w-full h-screen bg-no-repeat'>
+        <div>
+            <div className='relative w-full h-[400px] md:h-[500px] lg:h-screen bg-no-repeat'>
+                {/* banner bg image */}
                 <img
                     src={banner}
                     alt="Banner"
-                    className="lg:w-full lg:h-full  object-cover bg-center"
+                    className="w-full h-full object-cover bg-center"
                 />
+
+                {/* ====== Text Section ====== */}
+                <div className='absolute inset-0 pt-12 lg:pt-0 flex flex-col items-center justify-center text-center md:items-start md:text-left md:justify-end md:max-w-7xl md:mx-auto lg:bottom-1/5 md:bottom-1/9 md:px-8 lg:px-2 space-y-4 md:space-y-8'>
+                    <h1 className='text-[#FF8D29] text-2xl sm:text-3xl md:text-4xl eat'>
+                        Eat Sleep and
+                    </h1>
+
+                    <p className='text-xl sm:text-2xl md:text-4xl lg:text-7xl text-[#F1D6C6] -tracking-wider font-black'>
+                        Supper Delicious <br className='hidden md:block' /> Burger in Town
+                    </p>
+
+                    <p className='text-sm sm:text-base md:text-lg lg:text-2xl px-4 md:px-0 text-[#E8CCBB]'>
+                        Food is any substance consumed to provide nutritional <br className='hidden md:block' /> support for an organism
+                    </p>
+
+                    <button className='bg-[#FF9D2E] py-2 px-5 sm:py-2.5 sm:px-7 text-white hover:text-black hover:bg-white font-semibold'>
+                        Book a Table
+                    </button>
+                </div>
+
+                {/* ====== Right Images (only md and up) ====== */}
                 <img
                     src={rightEnd}
                     alt="rightEnd"
-                    className="absolute bottom-0 right-0 "
+                    className="hidden md:block absolute bottom-0 right-0 h-1/2 md:h-full"
                 />
-                {/* text */}
-                <div className='max-w-7xl mx-auto '>
-                    <div className='absolute  bottom-1/4 space-y-8'>
-                        <h1 className='text-[#FF8D29] text-4xl eat '>Eat Sleep and</h1>
-                        <p className='text-7xl text-[#F1D6C6] -tracking-wider font-black'>Supper Delicious <br />Burger in Town</p>
-                        <p className='text-[#E8CCBB] text-2xl'>Food is any substance consumed to provide nutritional <br /> support for an organism </p>
-                        <button className='bg-[#FF9D2E] py-2.5 px-7 text-white hover:text-black hover:bg-white font-semibold'>Book a Table</button>
-                    </div>
-                </div>
-                <img src={rightEnd2}
+                <img
+                    src={rightEnd2}
                     alt="right"
-                    className="absolute w-3/5 -bottom-14  -right-28 " />
-                <img src={burger}
-                    alt="right"
-                    className="absolute bottom-0 w-3/7 right-18 " />
+                    className="hidden md:block absolute lg:w-1/2 md:w-96 md:bottom-10 lg:bottom-0 lg:h-[90%] object-cover right-0"
+                />
+                <img
+                    src={burger}
+                    alt="burger"
+                    className="hidden md:block absolute md:bottom-10 lg:bottom-0 md:w-2/5 lg:right-28 md:right-8"
+                />
             </div>
         </div>
     );
--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { IoIosSearch } from 'react-icons/io';
+import { FiMenu, FiX } from 'react-icons/fi';
 import { IoSearchSharp } from 'react-icons/io5';
 import { Link } from 'react-router';
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
-     useEffect(() => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 50) {
                 setIsScrolled(true);
@@ -17,15 +18,15 @@ const Navbar = () => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
     return (
-        <nav className={`fixed top-0 left-0 w-full z-50 border-b-[1px] border-[#f5efef32] transition-all duration-300 ${isScrolled ? " bg-white" : "bg-transparent"
-                }`}>
+        <nav className={`fixed top-0 left-0 w-full z-50 border-b-[1px] border-[#f5efef32] transition-all duration-300 px-4 lg:px-2 md:px-8 ${isScrolled ? " bg-white" : "bg-transparent"
+            }`}>
 
             <div className='max-w-7xl mx-auto flex  items-center justify-between py-5'>
                 <div>
-                    <h1 className={`text-[28px]  font-semibold ${isScrolled ? "text-black" : "text-white"}`}>Foodie Haven<span className='text-[#FF8D29]'>.</span></h1>
+                    <h1 className={`md:text-[28px] text-2xl font-semibold ${isScrolled ? "text-black" : "text-white"}`}>Foodie Haven<span className='text-[#FF8D29]'>.</span></h1>
                 </div>
                 <div className='flex items-center gap-8'>
-                    <div>
+                    <div className='hidden lg:block'>
                         <ul className={`flex  font-semibold gap-8 ${isScrolled ? "text-black" : "text-[#F1D6C6] "}`}>
                             <Link to='/' className='hover:text-[#FF8D29]'>Home</Link>
                             <Link to='/' className='hover:text-[#FF8D29]'>About Us</Link>
@@ -34,17 +35,36 @@ const Navbar = () => {
                             <Link to='/' className='hover:text-[#FF8D29]'>Contact Us</Link>
                         </ul>
                     </div>
-                    <div className='flex items-center gap-4'>
+                    <div className='flex relative md:left-32 lg:left-0 items-center gap-4 '>
                         {/* <IoIosSearch className='text-2xl text-[#F1D6C6] font-extrabold'></IoIosSearch> */}
-                        <IoSearchSharp className={`text-2xl ${isScrolled ? "text-black" : "text-[#F1D6C6]"}`}></IoSearchSharp>
-                        <button class="btn relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-[#FF8D29]  group py-2.5 px-9">
-                            <span class="w-56 h-48 rounded bg-black absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                            <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-white font-semibold">Reservation</span>
-                        </button>
+                        <IoSearchSharp className={`text-2xl hidden md:block ${isScrolled ? "text-black" : "text-[#F1D6C6]"}`}></IoSearchSharp>
+                        <div className='hidden md:block'>
+                            <button class="btn  relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-[#FF8D29]  group py-2.5 px-9">
+                                <span class="w-56 h-48 rounded bg-black absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative  w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-white font-semibold">Reservation</span>
+                            </button>
+                        </div>
                     </div>
-
                 </div>
+
+                {/* Mobile & Tablet Menu Icon */}
+                <div className={`lg:hidden text-2xl cursor-pointer ${isScrolled ? "text-black" : "text-white"}`} onClick={() => setMenuOpen(!menuOpen)}>
+                    {menuOpen ? <FiX /> : <FiMenu />}
+                </div>
+
             </div>
+
+            {/* Mobile & Tablet Menu */}
+            {menuOpen && (
+                <div className="lg:hidden bg-white text-[#0D1216] font-semibold px-7 py-5 space-y-4">
+                    <ul className="flex flex-col gap-4 text-[14px]">
+                        {["Home", "About Us", "Shop", "Blog", "Contact Us"].map((item, i) => (
+                            <li key={i} className="cursor-pointer border-b-1 py-1 border-b-orange-100">{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+
         </nav>
     );
 };
